293f9c69961d5bee368d11861fde64b28fce6b18
Felipe <felipedossantosflor@gmail.com>
2022-07-19 18:31:48 -0300

    2º Exercicio

---
+++ ClasseEntrega/ClasseEntrega/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ClasseEntrega.Src;
+
+namespace ClasseEntrega
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Filmes piratasDoCaribe = new Filmes("Piratas do Caribe", "Ação", 10, 200, "Disney", "Inglês",
+                "3D");
+        }
+    }
+}
---
+++ ClasseEntrega/ClasseEntrega/Src/Filmes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseEntrega.Src
+{
+    public class Filmes
+    {
+        
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public int Classificacao { get; set; }
+        public int Duracao { get; set; }
+        public string Estudio { get; set; }
+        public string Idioma { get; set; }
+        public string tipoReproducao { get; set; }
+
+        public Filmes(string titulo, string genero, 
+            int classificacao, int duracao, string estudio, string idioma, string tipoReproducao)
+        {
+            Titulo = titulo;
+            Genero = genero;
+            Classificacao = classificacao;
+            Duracao = duracao;
+            Estudio = estudio;
+            Idioma = idioma;
+            this.tipoReproducao = tipoReproducao;
+        }
+
+        public override string ToString()
+        {
+            return $"\n Título do filme: {Titulo} + " +
+                $"Genero: {Genero}" +
+                $"Classificação Indicativa: {Classificacao}" +
+                $"Duração: {Duracao}" +
+                $"Estúdio: {Estudio}" +
+                $"Idioma: {Idioma}" +
+                $"Tipo de Reprodução: {this.tipoReproducao}";
+        }
+    }
+
+    public enum Categoria
+    {
+        COMEDIA, ROMANCE, FICCAO, ACAO, SUSPENSE
+    }
+}
